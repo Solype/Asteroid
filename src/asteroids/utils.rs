0301e4490c4f52d7872ecbc0652fdf11d39f3492
Popochounet <adrien.audiard@epitech.eu>
2025-10-27 13:27:39 +0100

    add(asteroid): spawn mechanic + spawning/despawning animation

---
+++ src/asteroids/utils.rs
@@ -0,0 +1,22 @@
+use rand_distr::{Normal, Distribution};
+use rand::Rng;
+
+pub fn sample_truncated_norm(
+    mean: f32,
+    std_dev: f32,
+    lower: f32,
+    upper: f32,
+    rng: &mut impl Rng,
+) -> f32 {
+    let dist = Normal::new(mean, std_dev).unwrap();
+    loop {
+        let v = dist.sample(rng);
+        if v >= lower && v <= upper {
+            return v;
+        }
+    }
+}
+
+pub fn f(x: f32) -> f32 {
+    2.0 * (- (x / 2.0 - 2.5).tanh() + 2.0)
+}
