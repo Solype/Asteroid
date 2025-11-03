730fe7bcd91970912e581fa973f4539953e64810
Popochounet <adrien.audiard@epitech.eu>
2025-11-03 07:32:56 +0100

    evol(sun): sun is spawn in asteroid setup

--- src/asteroids/mod.rs
+++ src/asteroids/mod.rs
@@ -22,9 +22,9 @@ pub struct AsteroidAssets {
 #[derive(Resource)]
 pub struct AsteroidConfig {
     max_asteroid: usize,
-    size_range:(f32, f32),
-    spawn_range:f32,
-    despawn_range:f32
+    size_range: (f32, f32),
+    spawn_range: f32,
+    despawn_range: f32,
 }
 
 const ANIMATION_DURATION: f32 = 0.5;
@@ -43,25 +43,24 @@ pub struct AsteroidPlugin;
 
 impl Plugin for AsteroidPlugin {
     fn build(&self, app: &mut App) {
-        app
-        .add_systems(Startup, setup)
-        .add_systems(
-            Update,
-            (
-                movement::move_asteroids,
-                spawn::asteroid_wave,
-                spawn::animate_spawn,
-                spawn::animate_despawn,
-                spawn::clear_asteroid
-                // collision::asteroid_collision_system,
-            ),
-        ).
-        insert_resource(AsteroidConfig {
-            max_asteroid: 50,
-            size_range: (1.0, 10.0),
-            spawn_range: 300.0,
-            despawn_range: 350.0,
-        });
+        app.add_systems(Startup, setup)
+            .add_systems(
+                Update,
+                (
+                    movement::move_asteroids,
+                    collision::asteroid_asteroid_collision,
+                    spawn::asteroid_wave,
+                    spawn::animate_spawn,
+                    spawn::animate_despawn,
+                    spawn::clear_asteroid, // collision::asteroid_collision_system,
+                ),
+            )
+            .insert_resource(AsteroidConfig {
+                max_asteroid: 50,
+                size_range: (1.0, 10.0),
+                spawn_range: 300.0,
+                despawn_range: 350.0,
+            });
     }
 }
 
@@ -87,4 +86,38 @@ pub fn setup(
         mesh: sphere_mesh,
         materials: materials_vec,
     });
+
+    let sun_position = Vec3::new(-1000.0, 1000.0, 0.0);
+    let sun_material = materials.add(StandardMaterial {
+        base_color: Color::srgb(0.988, 0.482, 0.0667),
+        emissive: Color::srgb(0.988, 0.482, 0.0667).to_linear() * 2.0,
+        unlit: false,
+        ..Default::default()
+    });
+    commands.spawn((
+        PointLight {
+            intensity: 1_000_000_000_000.0,
+            range: 50000.0,
+            radius: 500.0,
+            color: Color::srgb(1.0, 0.9, 0.0),
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_translation(sun_position),
+        children![(
+            Mesh3d(meshes.add(Sphere::new(500.0).mesh().uv(64, 64))),
+            MeshMaterial3d(sun_material),
+        )],
+    ));
+    commands.spawn((
+        Mesh3d(meshes.add(Sphere::new(520.0).mesh().uv(64, 64))), // slightly larger
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Color::srgba(1.0, 0.8, 0.2, 0.2), // faint orange glow
+            emissive: Color::srgb(1.0, 0.7, 0.2).to_linear() * 5.0,
+            unlit: true,
+            alpha_mode: AlphaMode::Add, // additive blending for glow effect
+            ..Default::default()
+        })),
+        Transform::from_translation(sun_position),
+    ));
 }
