e71dca8d9ffb03ae9e8caaeac56c7149bbd0c20f
Zaotic <zaotic-git.r2rv7@passmail.net>
2025-11-24 17:44:42 +0900

    feat(cam_system): move cam only when free look button is pressed

--- src/controller.rs
+++ src/controller.rs
@@ -126,20 +126,25 @@ fn player_system(
 fn player_cam_system(
     accumulated_mouse_motion: Res<AccumulatedMouseMotion>,
     player: Single<(&mut Transform, &CameraSensitivity), With<PlayerCam>>,
+    keybinds: Res<Keybinds>,
+    keyboard: Res<ButtonInput<KeyCode>>,
+    mouse: Res<ButtonInput<MouseButton>>,
 ) {
-    let (mut transform, camera_sensitivity) = player.into_inner();
-    let delta = accumulated_mouse_motion.delta;
+    if keybinds.free_look.pressed(&keyboard, &mouse) {
+        let (mut transform, camera_sensitivity) = player.into_inner();
+        let delta = accumulated_mouse_motion.delta;
 
-    if delta != Vec2::ZERO {
-        let delta_yaw = -delta.x * camera_sensitivity.x;
-        let delta_pitch = -delta.y * camera_sensitivity.y;
-        let (yaw, pitch, roll) = transform.rotation.to_euler(EulerRot::YXZ);
-        let mut yaw = yaw + delta_yaw;
-        let mut pitch = pitch + delta_pitch;
-        // const PITCH_LIMIT: f32 = FRAC_PI_2 - 0.01;
-        // pitch = pitch.clamp(-PITCH_LIMIT, PITCH_LIMIT);
-        // const YAW_LIMIT: f32 = FRAC_PI_2;
-        // yaw = yaw.clamp(-YAW_LIMIT, YAW_LIMIT);
-        transform.rotation = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
+        if delta != Vec2::ZERO {
+            let delta_yaw = -delta.x * camera_sensitivity.x;
+            let delta_pitch = -delta.y * camera_sensitivity.y;
+            let (yaw, pitch, roll) = transform.rotation.to_euler(EulerRot::YXZ);
+            let mut yaw = yaw + delta_yaw;
+            let mut pitch = pitch + delta_pitch;
+            const PITCH_LIMIT: f32 = FRAC_PI_2 - 0.01;
+            pitch = pitch.clamp(-PITCH_LIMIT, PITCH_LIMIT);
+            const YAW_LIMIT: f32 = FRAC_PI_2;
+            yaw = yaw.clamp(-YAW_LIMIT, YAW_LIMIT);
+            transform.rotation = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
+        }
     }
 }
