86ebb27ae31f293a578513b0b267a37dfb390744
Solype <ambroise.jacquemet@laposte.net>
2025-12-01 17:18:23 +0100

    feat(movement): added the inertia

--- src/controller.rs
+++ src/controller.rs
@@ -14,10 +14,10 @@ pub fn plugin(app: &mut App) {
     app.add_systems(
         Update,
         (player_system,
-         direction_controller::mouse_system,
-         direction_controller::rotate_spaceship,
-         direction_controller::roll_spaceship,
-         direction_controller::trans_spaceship
+            direction_controller::mouse_system,
+            direction_controller::rotate_spaceship,
+            direction_controller::roll_spaceship,
+            move_player_system
         )
             .in_set(GameSystemSet)
             .run_if(in_state(GameState::Game)),
@@ -37,14 +37,9 @@ pub struct PlayerCam;
 #[derive(Debug, Component, Deref, DerefMut)]
 pub struct CameraSensitivity(Vec3);
 
-#[derive(Component, Deref, DerefMut)]
-pub struct TranslationalVelocity(Vec3);
 
 #[derive(Component, Deref, DerefMut)]
 pub struct RotationalVelocity(Vec3);
-
-#[derive(Component, Deref, DerefMut)]
-pub struct MouseVector(Vec2);
 
 impl Default for CameraSensitivity {
     fn default() -> Self {
@@ -52,21 +47,9 @@ impl Default for CameraSensitivity {
     }
 }
 
-impl Default for TranslationalVelocity {
-    fn default() -> Self {
-        Self(Vec3::ZERO)
-    }
-}
-
 impl Default for RotationalVelocity {
     fn default() -> Self {
         Self(Vec3::ZERO)
-    }
-}
-
-impl Default for MouseVector {
-    fn default() -> Self {
-        Self(Vec2::ZERO)
     }
 }
 
@@ -90,7 +73,6 @@ fn player_system(
     if keybinds.free_look.pressed(&keyboard, &mouse) {
         let (mut transform, camera_sensitivity) = player.into_inner();
         let delta = accumulated_mouse_motion.delta;
-
         if delta != Vec2::ZERO {
             let delta_yaw = -delta.x * camera_sensitivity.x;
             let delta_pitch = -delta.y * camera_sensitivity.y;
@@ -103,5 +85,48 @@ fn player_system(
             yaw = yaw.clamp(-YAW_LIMIT, YAW_LIMIT);
             transform.rotation = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
         }
+    }
+}
+
+fn move_player_system(
+    time: Res<Time>,
+    keybinds: Res<Keybinds>,
+    keyboard: Res<ButtonInput<KeyCode>>,
+    mouse: Res<ButtonInput<MouseButton>>,
+    player: Single<(&mut Transform, &mut crate::asteroids::Velocity), With<Player>>,
+) {
+    let mut speed_to_add = Vec3::default();
+    let (mut transform, mut velocity) = player.into_inner();
+
+    if keybinds.right.pressed(&keyboard, &mouse) {
+        speed_to_add.x += 1.0_f32;
+    }
+    if keybinds.left.pressed(&keyboard, &mouse) {
+        speed_to_add.x += -1.0_f32;
+    }
+    if keybinds.forward.pressed(&keyboard, &mouse) {
+        speed_to_add.z += -1.0_f32;
+    }
+    if keybinds.backward.pressed(&keyboard, &mouse) {
+        speed_to_add.z += 1.0_f32;
+    }
+    if keybinds.up.pressed(&keyboard, &mouse) {
+        speed_to_add.y += 1.0_f32;
+    }
+
+    if keybinds.down.pressed(&keyboard, &mouse) {
+        speed_to_add.y += -1.0_f32;
+    }
+
+    if speed_to_add.length_squared() == 0.0 {
+        return;
+    }
+    speed_to_add = speed_to_add.normalize() * 2.0;
+    speed_to_add *= time.delta_secs();
+    speed_to_add = transform.rotation * speed_to_add;
+    transform.translation += speed_to_add * time.delta_secs();
+    velocity.0 += speed_to_add;
+    if velocity.0.length() > 10.0_f32 {
+        velocity.0 = velocity.0.normalize() * 10.0_f32;
     }
 }
