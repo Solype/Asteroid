db1cec84cd45165bb2931076021f3d50ddebed4d
Solype <ambroise.jacquemet@laposte.net>
2025-12-01 17:18:23 +0100

    feat(movement): added the inertia

--- src/asteroids/movement.rs
+++ src/asteroids/movement.rs
@@ -2,7 +2,7 @@ use crate::asteroids::*;
 
 pub fn move_asteroids(
     time: Res<Time>,
-    mut query: Query<(&mut Transform, &Velocity), With<Asteroid>>,
+    mut query: Query<(&mut Transform, &Velocity)>,
 ) {
     for (mut transform, velocity) in &mut query {
         transform.translation += **velocity * time.delta_secs();
