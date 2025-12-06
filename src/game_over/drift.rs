48177e715e7f749754ed9e867084d7a1be19c96f
Popochounet <adrien.audiard@epitech.eu>
2025-12-06 17:46:21 +0100

    evol(gameover): added drift

--- src/game_over/drift.rs
+++ src/game_over/drift.rs
@@ -1,4 +1,21 @@
+use rand::Rng;
+
+use crate::controller::structs::ControllerState;
 use crate::game_over::*;
+use crate::physics::{RotationVelocity, Velocity};
+
+pub fn setup_drift(player: Single<(&mut Velocity, &mut RotationVelocity), With<Player>>) {
+    let (mut vel, mut rot) = player.into_inner();
+
+    let mut rng = rand::rng();
+
+    **vel *= 20.;
+    *rot = RotationVelocity(Vec3::new(
+        rng.random_range(-2.0..2.0),
+        rng.random_range(-2.0..2.0),
+        rng.random_range(-2.0..2.0),
+    ))
+}
 
 pub fn run_drift(
     time: Res<Time>,
@@ -6,13 +23,14 @@ pub fn run_drift(
     mut next_menu_state: ResMut<NextState<MenuState>>,
     mut next_game_state: ResMut<NextState<GameState>>,
     mut next_gameover_state: ResMut<NextState<GameOverState>>,
+    mut next_controller_state: ResMut<NextState<ControllerState>>,
 ) {
     if !timer.timer.is_finished() {
         timer.timer.tick(time.delta());
-        // todo Drift logic
     } else {
         next_menu_state.set(MenuState::GameOver);
         next_game_state.set(GameState::Menu);
         next_gameover_state.set(GameOverState::Menu);
+        next_controller_state.set(ControllerState::None);
     }
 }
--- src/game_over/mod.rs
+++ src/game_over/mod.rs
@@ -1,8 +1,12 @@
 use bevy::prelude::*;
 
 use crate::{
-    controller::structs::Player, game_states::GameState, menu::structs::MenuState,
-    menu::systems::focus_main_screen,
+    controller::structs::{ControllerState, Player},
+    game_over::drift::setup_drift,
+    game_states::GameState,
+    globals_structs::Score,
+    menu::{structs::MenuState, systems::focus_main_screen},
+    physics::{RotationVelocity, Velocity},
 };
 
 mod drift;
@@ -32,7 +36,7 @@ pub struct GameOverPlugin;
 impl Plugin for GameOverPlugin {
     fn build(&self, app: &mut App) {
         app.init_state::<GameOverState>()
-            .add_systems(OnEnter(GameOverState::Drift), setup)
+            .add_systems(OnEnter(GameOverState::Drift), (setup, setup_drift))
             .add_systems(
                 Update,
                 blink_lights.run_if(not(in_state(GameOverState::None))),
@@ -46,7 +50,13 @@ impl Plugin for GameOverPlugin {
     }
 }
 
-fn setup(mut commands: Commands, player: Single<Entity, With<Player>>) {
+fn setup(
+    mut commands: Commands,
+    player: Single<Entity, With<Player>>,
+    mut next_controller_state: ResMut<NextState<ControllerState>>,
+) {
+    next_controller_state.set(ControllerState::FreeLook);
+
     commands.insert_resource(DriftTimer {
         timer: Timer::from_seconds(3.0, TimerMode::Once),
     });
@@ -55,20 +65,20 @@ fn setup(mut commands: Commands, player: Single<Entity, With<Player>>) {
         parent_builder.spawn((
             DespawnOnExit(MenuState::GameOver),
             PointLight {
-                intensity: 1_000_000.0,
-                range: 10.0,
-                radius: 10.0,
+                intensity: 25_000.0,
+                range: 5.0,
+                radius: 3.0,
                 color: Color::srgb(1.0, 0.0, 0.0),
                 shadows_enabled: true,
                 ..default()
             },
             Transform {
-                translation: Vec3::new(1.0, 0.5, 0.5),
+                translation: Vec3::new(0.35, 1.2, -0.5),
                 ..default()
             },
             BlinkingLight {
                 timer: Timer::from_seconds(0.7, TimerMode::Repeating),
-                on_intensity: 1_000_000.0,
+                on_intensity: 25_000.0,
                 off_intensity: 0.0,
                 is_on: true,
             },
@@ -77,19 +87,19 @@ fn setup(mut commands: Commands, player: Single<Entity, With<Player>>) {
             DespawnOnExit(MenuState::GameOver),
             PointLight {
                 intensity: 0.0,
-                range: 10.0,
-                radius: 10.0,
+                range: 5.0,
+                radius: 3.0,
                 color: Color::srgb(1.0, 0.0, 0.0),
                 shadows_enabled: true,
                 ..default()
             },
             Transform {
-                translation: Vec3::new(-1.0, 0.5, 0.5),
+                translation: Vec3::new(-0.35, 1.2, -0.5),
                 ..default()
             },
             BlinkingLight {
                 timer: Timer::from_seconds(0.7, TimerMode::Repeating),
-                on_intensity: 1_000_000.0,
+                on_intensity: 25_000.0,
                 off_intensity: 0.0,
                 is_on: false,
             },
@@ -113,6 +123,14 @@ fn blink_lights(time: Res<Time>, mut lights: Query<(&mut PointLight, &mut Blinki
     }
 }
 
-fn reset_gameover_state(mut next_gameover_state: ResMut<NextState<GameOverState>>) {
+fn reset_gameover_state(
+    mut next_gameover_state: ResMut<NextState<GameOverState>>,
+    mut score: ResMut<Score>,
+    mut player: Single<(&mut Velocity, &mut RotationVelocity), With<Player>>,
+) {
     next_gameover_state.set(GameOverState::None);
+
+    score.value = 0;
+    **player.0 = Vec3::ZERO;
+    **player.1 = Vec3::ZERO;
 }
